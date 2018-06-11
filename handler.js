32b98bce8bf0877aa2a85db655a2e6a4324f821d
Julian Tellez <juliantellezmendez@gmail.com>
2018-06-11 22:12:36 +0100

    creating handler

--- handler.js
+++ handler.js
@@ -1,16 +1,13 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const markdownParser = require('./src/functions/markdownParser')
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+const parseToHtml = (event, context, callback) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(markdownParser(event)),
+    };  
+    callback(null, response);
 };
+
+module.exports.parseToHtml = parseToHtml
